cc11ec08262f5edd841276ace92393caf7299f9f
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:17:41 +0700

    Update LibraryFinder.hpp

--- include/Game/LibraryFinder.hpp
+++ include/Game/LibraryFinder.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <cstdint>
+#include <string>
+#include <uintptr_t.h>
 
-namespace PHX
-{
-    class LibraryFinder
-    {
-    public:
-        static uintptr_t Find(const char* libraryName);
-    };
-}
+namespace PHX {
+
+class LibraryFinder {
+public:
+    static uintptr_t FindLibrary(const std::string& libName);
+};
+
+} // namespace PHX
