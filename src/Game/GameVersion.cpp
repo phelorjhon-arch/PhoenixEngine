63a18d2d9c70e516fe1fc8bbd9cc20d50cc8ad2c
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:22:27 +0700

    Update GameVersion.cpp

--- src/Game/GameVersion.cpp
+++ src/Game/GameVersion.cpp
@@ -1,17 +1,22 @@
 #include "Game/GameVersion.hpp"
+#include "Game/ProcMapsReader.hpp"
 
-namespace PHX
-{
-    std::string GameVersion::mVersion = "Unknown";
+namespace PHX {
 
-    bool GameVersion::Detect()
-    {
-        mVersion = "GTA SA AML ARM64";
-        return true;
+GTAVersion GameVersion::DetectVersion() {
+    uintptr_t libGTASABase = ProcMapsReader::GetModuleBase("libGTASA.so");
+    if (libGTASABase != 0) {
+        return GTAVersion::GTA_SA_2_00;
     }
+    return GTAVersion::Unknown;
+}
 
-    std::string GameVersion::GetVersion()
-    {
-        return mVersion;
+std::string GameVersion::GetVersionName(GTAVersion version) {
+    switch (version) {
+        case GTAVersion::GTA_SA_2_00: return "GTA:SA v2.00";
+        case GTAVersion::GTA_SA_2_10: return "GTA:SA v2.10";
+        default: return "Unknown";
     }
 }
+
+} // namespace PHX
