056fa33fecbefd5273fb4b6b5240c8ad0fe7ac47
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:09:00 +0700

    Update Logger.hpp

--- include/Core/Logger.hpp
+++ include/Core/Logger.hpp
@@ -1,9 +1,23 @@
 #pragma once
+
+#include <string>
+
 namespace PHX {
-class Logger{
-public:
- static void Info(const char*);
- static void Warn(const char*);
- static void Error(const char*);
+
+enum class LogLevel {
+    Info,
+    Warning,
+    Error,
+    Debug
 };
-}
+
+class Logger {
+public:
+    static void Log(LogLevel level, const std::string& message);
+    static void Info(const std::string& message);
+    static void Warn(const std::string& message);
+    static void Error(const std::string& message);
+    static void Debug(const std::string& message);
+};
+
+} // namespace PHX
