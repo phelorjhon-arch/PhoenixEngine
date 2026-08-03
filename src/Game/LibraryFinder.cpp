b346db36f3707f775bf30b42f825d4f2adb86d5a
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:23:16 +0700

    Update LibraryFinder.cpp

--- src/Game/LibraryFinder.cpp
+++ src/Game/LibraryFinder.cpp
@@ -1,34 +1,10 @@
 #include "Game/LibraryFinder.hpp"
+#include "Game/ProcMapsReader.hpp"
 
-#include <cstdio>
-#include <cstring>
+namespace PHX {
 
-namespace PHX
-{
-    uintptr_t LibraryFinder::Find(const char* libraryName)
-    {
-        if (!libraryName)
-            return 0;
-
-        FILE* maps = fopen("/proc/self/maps", "r");
-
-        if (!maps)
-            return 0;
-
-        char line[512];
-
-        while (fgets(line, sizeof(line), maps))
-        {
-            if (strstr(line, libraryName))
-            {
-                uintptr_t base = 0;
-                sscanf(line, "%lx-", &base);
-                fclose(maps);
-                return base;
-            }
-        }
-
-        fclose(maps);
-        return 0;
-    }
+uintptr_t LibraryFinder::FindLibrary(const std::string& libName) {
+    return ProcMapsReader::GetModuleBase(libName);
 }
+
+} // namespace PHX
