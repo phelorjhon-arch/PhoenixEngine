640de8e122fd960ab5e1d7598ec0573c633b562e
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:13:57 +0700

    Update MemoryMap.hpp

--- include/Game/MemoryMap.hpp
+++ include/Game/MemoryMap.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <uintptr_t.h>
 
-namespace PHX
-{
-    class MemoryMap
-    {
-    public:
-        static bool Refresh();
-        static bool Contains(const std::string& module);
-    };
-}
+namespace PHX {
+
+struct MapEntry {
+    uintptr_t startAddress;
+    uintptr_t endAddress;
+    std::string permissions;
+    std::string path;
+};
+
+class ProcMapsReader {
+public:
+    static std::vector<MapEntry> Read();
+    static uintptr_t GetModuleBase(const std::string& moduleName);
+};
+
+} // namespace PHX
