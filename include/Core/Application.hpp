9447f36a6f01cfde218ab8d2c0290908136d826c
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:07:17 +0700

    Update Application.hpp

--- include/Core/Application.hpp
+++ include/Core/Application.hpp
@@ -1,7 +1,24 @@
 #pragma once
+
+#include <string>
+#include <memory>
+
 namespace PHX {
-class Application{
+
+class Application {
 public:
-    bool Start();
+    Application();
+    ~Application();
+
+    bool Initialize(const std::string& configPath);
+    void Run();
+    void Shutdown();
+
+    bool IsRunning() const { return m_isRunning; }
+
+private:
+    bool m_isRunning{false};
+    std::string m_configPath;
 };
-}
+
+} // namespace PHX
