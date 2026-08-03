e921f1b9a657387174f5a484a5a99ecae89d20ad
phelorjhon-arch <phelorjhon@gmail.com>
2026-08-03 15:18:46 +0700

    Update ModuleScanner.hpp

--- include/Game/ModuleScanner.hpp
+++ include/Game/ModuleScanner.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <cstdint>
+#include <string>
+#include <uintptr_t.h>
 
-namespace PHX
-{
-    class ModuleScanner
-    {
-    public:
-        static bool Scan();
-        static uintptr_t GetBase();
+namespace PHX {
 
-    private:
-        static uintptr_t mBase;
-    };
-}
+class ModuleScanner {
+public:
+    static uintptr_t ScanPattern(uintptr_t base, size_t size, const char* pattern, const char* mask);
+};
+
+} // namespace PHX
